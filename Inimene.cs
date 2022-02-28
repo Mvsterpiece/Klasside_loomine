bc4b48301863a50d4c7c74c0acba8ee636f7de43
Opilane TTHK <opilane@tthk.ee>
2022-02-28 10:05:11 +0200

    Add project files.

---
+++ Inimene.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Klasside_loomine
+{
+	class Inimene
+	{
+		public string perenimi;
+		public string staatus;
+		int vanus;
+		public int Palk;
+		public Inimene() { }
+		public Inimene(string Perenimi)
+		{
+			perenimi = Perenimi;
+		}
+		public string Perenimi
+		{
+			set { if (perenimi == null) perenimi = value; }
+			get { return perenimi; }
+		}
+		public int Vanus
+		{
+			set
+			{
+				vanus = value;
+				if (vanus<7)
+				{
+					staatus = "Laps";
+				}
+				else if (vanus<17)
+				{
+					staatus = "koolilaps";
+				}
+				else if (vanus<24)
+				{
+					staatus = "ülikoolilaps";
+				}
+				else
+				{
+					staatus = "tööline";
+				}
+			}
+			get { return vanus; }
+
+		}
+		public string Staatus
+		{
+			get { return staatus; }
+		}
+		public void Tervitamine()
+		{
+			Console.WriteLine("Minu perenimi on "+ perenimi);
+			Console.WriteLine("Ma olen {0} aastat vana, olen {1}", vanus, staatus);
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Klasside_loomine
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Inimene naine = new Inimene("Kask");
+			Console.WriteLine("Kui vana sa oled?");
+			naine.Vanus = int.Parse(Console.ReadLine());
+			naine.Tervitamine();
+			Inimene mees = new Inimene();
+			mees.Perenimi = "Shakur";
+			mees.Vanus = 45;
+			Console.WriteLine(mees.Perenimi+" on "+mees.Vanus +" aastane mees");
+			mees.Tervitamine();
+
+			Inimene[] inimesed = new Inimene[5];
+			for (int i = 0; i < 5; i++)
+			{
+				inimesed[i] = new Inimene();
+				Console.WriteLine("Mis on sinu nimi?");
+				inimesed[i].Perenimi = Console.ReadLine();
+				Console.WriteLine("Kui vana sa oled?");
+				inimesed[i].Vanus = int.Parse(Console.ReadLine());
+			}
+			foreach (Inimene inimene in inimesed)
+			{
+				inimene.Tervitamine();
+			}
+
+
+			Console.ReadLine();
+		}
+	}
+}
